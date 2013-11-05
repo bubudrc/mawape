3951b740b380c89a9fd5e1fdea0e2b3c35e56ad4
Lisandro Vico <lisandrovico@gmail.com>
2013-11-04 22:56:56 -0300

    UPDATED: Font awesome files, social icons, added modal functionality

--- js/main.js
+++ js/main.js
@@ -1,61 +1,22 @@
 $(function() {
-	
-	// Load movies
-	var loadMovies = function(moviesURL) {
-		
-		// Add "loading" spinner while fetching content
-		$('.main-cnt').addClass('cnt-loading');
-		
-		$.ajax({ 
-			type: "GET",
-			dataType: "JSON",
-			url: moviesURL,
-			success: function(data){
-				var movieList = '';
-				for (var i = 0; i < data.length; i++) {
-					var movieItem = '';
-					movieItem += '<li class="cnt-list-item">';
-					movieItem += '<span class="list-item-handler"><i class="icon-ellipsis-vertical"></i></span>';
-					movieItem += '<img class="movie-img" src="' + data[i].Image + '" width="50" height="75" />';
-					movieItem += '<div class="movie-info">';
-					movieItem += '<h3>' + data[i].Name + '</h3>';
-					movieItem += '<p><small><span class="movie-genre">' + data[i].Genre + '</span>, ';
-					movieItem += '<span class="movie-time">' + data[i].Length + ' min</span> ';
-					movieItem += '<span class="movie-rate">' + data[i].Rating + '</span></small>'
-					movieItem += data[i].LeadStars + '</p>';
-					movieItem += '</div><i class="icon-chevron-right"></i></li>';
 
-					// Add movie to movieList set
-					movieList += movieItem;
-				};
-				$('.cnt-list').html(movieList);
+	var modalWrapper = $(".modal-wrapper");
+	var modalContent = $(".modal-cnt");
 
-				// Remove "loading" spinner when content is already in place
-				$('.main-cnt').removeClass('cnt-loading');
-
-				// Reset scroll to top of the page
-				window.scrollTo(0,0);
-			}
-		});
-	}
-
-	// Load movies for default tab
-	loadMovies($('.tab-btn.tab-default').attr('href'));
-	
 	// Change active tab and load new movies list
-	$('.tab-btn').on('click', function(e){
+	$('.btn-contact').on('click', function(e){
 		e.preventDefault();
-		$('.tab-btn').removeClass('tab-active');
-		$(this).addClass('tab-active');
-		loadMovies($(this).attr('href'));
+		$(this).addClass('active');
+		modalWrapper.addClass('modal-open');
 	})
 
-	// jQuery UI Sortable list
-	$( ".cnt-list").sortable({
-		revert: true,
-		handle: ".list-item-handler",
-		opacity: 0.85
+	$(document).mouseup(function (e) {
+
+		// if the target of the click isn't the container nor a descendant of the container
+		if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+			modalWrapper.removeClass('modal-open');
+			$('.btn-contact').removeClass('active');
+		}
 	});
-	$( ".cnt-list" ).disableSelection();
 	
 });
